fc2e9a0b9e57016aa3927624e81e42c6c194d800
debijayaram <112899979+debijayaram@users.noreply.github.com>
2022-09-07 15:36:39 -0400

    Update streamlit_app.py
    fix else:

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
   if not fruit_choice:
     streamlit.write('The user entered ', fruit_choice)
-  else
+  else:
     #import requests
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     # take the json and display normailzed view
